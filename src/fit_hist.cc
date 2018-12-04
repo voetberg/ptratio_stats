3942ad16b7615af33e1f92b456a7e5b16df7cd78
voetber2 <voetber2@msu.edu>
2018-12-04 11:42:04 -0500

    A mostly functional fit

--- src/fit_hist.cc
+++ src/fit_hist.cc
@@ -5,17 +5,13 @@
 #include <chrono>             
 #include <complex> 
 
-#include <boost/optional.hpp> 
 #include <TFile.h>            
 #include <TChain.h>           
 #include <TH1.h>              
-#include <TLorentzVector.h>   
-#include <TTree.h>            
 #include <TMinuit.h>                               
                              
 
 using namespace std;          
-bool sort_pt (TLorentzVector i, TLorentzVector j){return (i.Pt()>j.Pt());}                         
 
 /*
 //Just a Legendre function 
@@ -36,74 +32,80 @@ double Legendre(double x, const double* c) {
 }
 */
 
-//Create the containers for the values 
-
-
-
-
+//Set Bins  
+Int_t nbins = 38; 
+vector<Double_t> hist_data; 
+Int_t npar = 4; 
 
 //Creates a function to fit to 
 //3rd deg polynomial
-Double_t funct(Double_t *x, Double_t *par){
-  Double_t val = par[0] + par[1]*x[0] + par[2]*x[0]*x[0] + par[3]*x[0]*x[0]*x[0]; 
+Double_t funct(Double_t x, Double_t *par){
+  Double_t val = par[0] + par[1]*x + par[2]*x*x + par[3]*x*x*x; 
   return val; 
 }
 
 //Returned the chi sq'd
 void chisq(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag){
-   const Int_t nbins = 38; //I just know this 
-   Int_t i;
 
-//calculate chisquare
+  //calculate chisquare
    Double_t chisq = 0;
-   Double_t delta;
-   for (i=0;i<nbins; i++) {
-     delta  = (z[i]-funct(x[i],y[i],par))/errorz[i];
-     chisq += delta*delta;
+   for (int i=0; i<nbins; i++) {
+     chisq += pow((hist_data[i]-funct(hist_data[i],par)),2)/hist_data[i];
    }
-   f = chisq;
+   f=chisq;
 }
 
 int main(){ 
   const vector<string>
-    num {300,350,400,400p}; 
+    num {"300","350","400","400p"}; 
 
   //Load in histogram 
-  TFile *file = new TFile("$TMPDIR/ptratio_data.root"); 
-  cout>>"Loaded in File">>endl; 
+  TFile *file = new TFile("/home/voetberg/voetberg/vari_dist/ptratio_data.root"); //Run from terminal
+  //TFile *file = new TFile("$TMPDIR/ptratio_data.root"); //Run from Condor
+  cout<<"Loaded in File"<<endl; 
   
   //Loop through each histogram in the file
-  for (int i=0, i<4, ++i){
-    TH1D *h_fit = (TH1F*) file->Get("Data_Signal__ptratio_"+num[i]); 
-    cout>>"Loaded in histogram: ">>h_fit->GetName()>>endl;
-    
+  for (int i=0; i<4; ++i){
+    TString name = "Data_Signal__ptratio_" + num[i]; 
+    TH1D *h_fit = (TH1D*) file->Get(name); 
+    cout<<"Loaded in histogram: "<<h_fit->GetName()<<endl;
+
+    //Set the data in a container
+    hist_data.reserve(nbins); 
+    //const Double_t bin_width = 20./nbins;
+    for (int i=1; i<=nbins; ++i){
+      Double_t x = h_fit->GetBinContent(i); 
+      hist_data.push_back(x); 
+    }
+    cout<<"Set data in vector"<<endl; 
+
+
     //Use Minuit
-    TMinuit *Minuit = new TMinuit(38); 
+    TMinuit *gMinuit = new TMinuit(nbins); 
     gMinuit->SetFCN(chisq); 
-    cout>>"Set Minuit">>endl;  
+    cout<<"Set Minuit"<<endl;  
 
     Double_t arglist[10]; 
     Int_t ier = 0; 
 
-    arglst[0]=1; 
+    arglist[0]=1; 
     gMinuit->mnexcm("SET ERR", arglist,1,ier); 
     
     //Set initial values 
-    static Double_t parm_start{50000,20000,20000,-1000}; 
+    static Double_t param_start[4] ={50000,20000,20000,-1000}; 
     static Double_t step = .01;
     gMinuit->mnparm(0, "a1", param_start[0], step, 0,0, ier); 
     gMinuit->mnparm(1, "a2", param_start[1], step, 0,0, ier); 
     gMinuit->mnparm(2, "a3", param_start[2], step, 0,0, ier); 
     gMinuit->mnparm(3, "a4", param_start[3], step, 0,0, ier); 
-    cout>>"Set intial parameters">>endl; 
+    cout<<"Set intial parameters"<<endl; 
 
     //Use migrad to minimize 
     arglist[0]=500; 
     arglist[1]=1;
-    gMinuit->mnexxcm("MIGRAD",arglist,2,ier); 
+    gMinuit->mnexcm("MIGRAD",arglist,2,ier); 
 
     //Then Print
-    cout>>h_fit->GetName()>>endl; 
     Double_t amin,edm,errdef;
     Int_t nvpar,nparx,icstat;
     gMinuit->mnstat(amin,edm,errdef,nvpar,nparx,icstat);
